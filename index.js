ae195dde5f486a32ee69f9e03028f1d3a3062799
Adones Balica <adonesbalica@gmail.com>
2022-03-04 15:43:08 -0300

    Estilo dos cards feito, adicionado um pouco de responsividade e trocamos de api

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 async function apiDada() {
-    const apiPokemos = fetch("https://pokeapi.co/api/v2/pokemon")
+    const apiPokemos = fetch("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
     const reponseApi = await apiPokemos
     return await reponseApi.json()
-
 }
 
 
 async function getPokemon() {
     let apiRepose = await apiDada()
-    for (let i = 0; i < apiRepose.results.length; i++) {
-        let name = apiRepose.results[i].name
-        mountComponentDiv(name)
-    }
 
+    // aqui vai ser incrementado a questão da páginação 
+    for (let i = 0; i < apiRepose.pokemon.length; i++) {
+        let name = await apiRepose.pokemon[i].name
+        let img = await apiRepose.pokemon[i].img
+        mountComponentDiv(name, img)
+    }
 }
+
+
 
 getPokemon()
 
 /*Montar as divs */
-function mountComponentDiv(name) {
+function mountComponentDiv(name, url) {
     const containerPokemos = document.querySelector('.container-pokemos')
-
     const div = document.createElement('div')
     const p = document.createElement('p')
+    const img = document.createElement('img')
+    div.classList.add('frame-pokemon')
+
     p.innerHTML = name
+    img.src = url
+    div.appendChild(img)
     div.appendChild(p)
     containerPokemos.appendChild(div)
 }
