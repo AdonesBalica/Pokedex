d9e59924fd80ec2627640b3ee4661f0b50b056bd
Adones Balica <adonesbalica@gmail.com>
2022-03-04 18:07:16 -0300

    Adicionado a função do campo de pesquisa dinâmico

--- index.js
+++ index.js
@@ -7,17 +7,32 @@ async function apiDada() {
 
 async function getPokemon() {
     let apiRepose = await apiDada()
-
     // aqui vai ser incrementado a questão da páginação onde tá o 24
     for (let i = 0; i < 24; i++) {
         let { name, img } = await apiRepose.pokemon[i]
         mountComponentDiv(name, img)
     }
 }
-
-
-
 getPokemon()
+
+
+/* paginação */
+const inputValue = document.querySelector('.input-search')
+
+inputValue.addEventListener('input', (e) => {
+    let inputText = e.target.value.toLowerCase()
+    let cardsPok = document.querySelectorAll('.frame-pokemon')
+    let namePokemon = document.querySelector('.pokemon-name')
+
+    cardsPok.forEach((element) => {
+        let title = element.querySelector('p')
+        if (title.textContent.toLocaleLowerCase().includes(inputText)) {
+            title.parentNode.style.display = 'flex'
+        } else {
+            title.parentNode.style.display = 'none'
+        }
+    });
+})
 
 /*Montar as divs */
 function mountComponentDiv(name, url) {
@@ -26,6 +41,7 @@ function mountComponentDiv(name, url) {
     const p = document.createElement('p')
     const img = document.createElement('img')
     div.classList.add('frame-pokemon')
+    p.classList.add('pokemon-name')
 
     p.innerHTML = name
     img.src = url
