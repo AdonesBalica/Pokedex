55c48c5fbacc055c9357f95849eb32d5f25e7d7d
Adones Balica <adonesbalica@gmail.com>
2022-03-05 09:41:34 -0300

    Adicionando paginação e o estilo do botão de carregar mais cards

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
 async function apiDada() {
-    const apiPokemos = fetch("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
-    const reponseApi = await apiPokemos
-    return await reponseApi.json()
+    const apiPokemos = fetch(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+    );
+    const reponseApi = await apiPokemos;
+    return await reponseApi.json();
 }
 
+const inputValue = document.querySelector(".input-search");
+const loadMorePosts = document.querySelector(".load-cards");
+
+let page = 0;
+let cardsPerPage = 10;
+let nextCard = 10;
+let nextPage = 10;
+
+function pagination() {
+    getPokemon()
+    nextPage = page + cardsPerPage + nextCard
+    page = page + cardsPerPage
+    return nextPage
+}
+
+loadMorePosts.addEventListener("click", () => {
+    pagination()
+})
 
 async function getPokemon() {
-    let apiRepose = await apiDada()
+    let apiRepose = await apiDada();
     // aqui vai ser incrementado a questão da páginação onde tá o 24
-    for (let i = 0; i < 24; i++) {
-        let { name, img } = await apiRepose.pokemon[i]
-        mountComponentDiv(name, img)
-    }
+
+    apiRepose.pokemon.slice(page, nextPage).forEach((e, i) => {
+        let name = e.name
+        let img = e.img
+        mountComponentDiv(name, img);
+    })
+
 }
 getPokemon()
 
-
-/* paginação */
-const inputValue = document.querySelector('.input-search')
-
-inputValue.addEventListener('input', (e) => {
-    let inputText = e.target.value.toLowerCase()
-    let cardsPok = document.querySelectorAll('.frame-pokemon')
-    let namePokemon = document.querySelector('.pokemon-name')
-
-    cardsPok.forEach((element) => {
-        let title = element.querySelector('p')
-        if (title.textContent.toLocaleLowerCase().includes(inputText)) {
-            title.parentNode.style.display = 'flex'
-        } else {
-            title.parentNode.style.display = 'none'
-        }
-    });
-})
-
 /*Montar as divs */
 function mountComponentDiv(name, url) {
-    const containerPokemos = document.querySelector('.container-pokemos')
-    const div = document.createElement('div')
-    const p = document.createElement('p')
-    const img = document.createElement('img')
-    div.classList.add('frame-pokemon')
-    p.classList.add('pokemon-name')
+    const containerPokemos = document.querySelector(".container-pokemos");
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+    div.classList.add("frame-pokemon");
+    p.classList.add("pokemon-name");
 
-    p.innerHTML = name
-    img.src = url
-    div.appendChild(img)
-    div.appendChild(p)
-    containerPokemos.appendChild(div)
+    p.innerHTML = name;
+    img.src = url;
+    div.appendChild(img);
+    div.appendChild(p);
+    containerPokemos.appendChild(div);
 }
-
-
