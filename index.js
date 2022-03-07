ea003ce69369ff6237779bc6cfa4df818faab5df
Adones Balica <adonesbalica@gmail.com>
2022-03-07 17:20:35 -0300

    Refatorando código no caso a requisição da api

--- index.js
+++ index.js
@@ -1,66 +1,32 @@
-async function apiDada() {
-    const apiPokemos = fetch(
-        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
-    );
-    const reponseApi = await apiPokemos;
-    return await reponseApi.json();
-}
+loadPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const inputValue = document.querySelector(".input-search");
-
-async function getPokemon() {
-    let apiRepose = await apiDada();
-    apiRepose.pokemon.forEach((e) => {
-        const { name, img, id } = e;
-        mountComponentDiv(name, img, id);
-    });
-}
-
-getPokemon();
-inputValue.addEventListener("input", async () => {
-    let filtrarPokemon = document.querySelectorAll(".frame-pokemon");
-    filtrarPokemon.forEach((element) => {
-        let name = element.children[1].textContent.toLowerCase();
-        if (name.includes(inputValue.value.toLowerCase())) {
-            element.style.display = "flex";
-        } else {
-            element.style.display = "none";
-        }
-    });
-});
-
-/*Montar as divs */
-function mountComponentDiv(name, url, id) {
-    const containerPokemos = document.querySelector(".container-pokemos");
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    const img = document.createElement("img");
-    div.classList.add("frame-pokemon");
-    div.id = id;
-    p.classList.add("pokemon-name");
-    img.classList.add("image-pokemon")
-
-    p.innerHTML = name;
-    img.src = url;
-    div.appendChild(p);
-    div.appendChild(img);
-    containerPokemos.appendChild(div);
-}
-
-/*Montar o modalzinho mané*/
-function mountModalComonent(event) {
-    console.log(event);
-}
-
-//Evento dos clicks para abrir o modalzin
-document.addEventListener("click", (event) => {
-    const element = event.target;
-    if (element.classList.contains("frame-pokemon")) {
-        mountModalComonent(element);
+    const pokemonPromises = [];
+    for (let i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(reponse => reponse.json()))
     }
-    if (element.classList.contains('pokemon-name') ||
-        element.classList.contains("image-pokemon")) {
-        mountModalComonent(element.parentNode)
-    }
-});
 
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            createCardPokemon(pokemons)
+        });
+}
+
+loadPokemon();
+
+
+createCardPokemon = (pokemons) => {
+    const ulContainer = document.querySelector('.ul-container')
+    for (let i = 1; i < 150; i++) {
+        const li = document.createElement('li')
+        const img = document.createElement('img')
+
+        const p = document.createElement('p')
+        img.src = pokemons[i].sprites.front_default;
+        p.innerHTML = pokemons[i].name
+
+        li.appendChild(img)
+        li.appendChild(p)
+        ulContainer.appendChild(li)
+    }
+}
