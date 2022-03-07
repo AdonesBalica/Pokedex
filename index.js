e56a9d99a55f1944cdebf7783fd0dde6b34c0fd4
Adones Balica <adonesbalica@gmail.com>
2022-03-07 13:23:59 -0300

    Adicionando favicon, adicionando evento de click nos cards para abrir o modal de estatística

--- index.js
+++ index.js
@@ -11,38 +11,56 @@ const inputValue = document.querySelector(".input-search");
 async function getPokemon() {
     let apiRepose = await apiDada();
     apiRepose.pokemon.forEach((e) => {
-        let name = e.name
-        let img = e.img
-        mountComponentDiv(name, img);
-    })
+        const { name, img, id } = e;
+        mountComponentDiv(name, img, id);
+    });
 }
 
-getPokemon()
-inputValue.addEventListener('input', async () => {
-    let filtrarPokemon = document.querySelectorAll('.frame-pokemon')
-
+getPokemon();
+inputValue.addEventListener("input", async () => {
+    let filtrarPokemon = document.querySelectorAll(".frame-pokemon");
     filtrarPokemon.forEach((element) => {
-        let name = element.children[1].textContent.toLowerCase()
+        let name = element.children[1].textContent.toLowerCase();
         if (name.includes(inputValue.value.toLowerCase())) {
-            element.style.display = 'flex'
+            element.style.display = "flex";
         } else {
-            element.style.display = 'none'
+            element.style.display = "none";
         }
-    })
-})
+    });
+});
 
 /*Montar as divs */
-function mountComponentDiv(name, url) {
+function mountComponentDiv(name, url, id) {
     const containerPokemos = document.querySelector(".container-pokemos");
     const div = document.createElement("div");
     const p = document.createElement("p");
     const img = document.createElement("img");
     div.classList.add("frame-pokemon");
+    div.id = id;
     p.classList.add("pokemon-name");
+    img.classList.add("image-pokemon")
 
     p.innerHTML = name;
     img.src = url;
-    div.appendChild(img);
     div.appendChild(p);
+    div.appendChild(img);
     containerPokemos.appendChild(div);
 }
+
+/*Montar o modalzinho mané*/
+function mountModalComonent(event) {
+    console.log(event);
+}
+
+//Evento dos clicks para abrir o modalzin
+document.addEventListener("click", (event) => {
+    const element = event.target;
+    if (element.classList.contains("frame-pokemon")) {
+        mountModalComonent(element);
+    }
+    if (element.classList.contains('pokemon-name') ||
+        element.classList.contains("image-pokemon")) {
+        mountModalComonent(element.parentNode)
+    }
+});
+
